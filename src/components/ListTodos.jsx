98e9bb78a0459bd44b8a8ea0a32a2cd2eb3a4d30
hopesf <gecin18@gmail.com>
2022-10-04 13:25:14 +0300

    ui ready

---
+++ src/components/ListTodos.jsx
@@ -0,0 +1,83 @@
+const ListTodos = () => {
+  return (
+    <div className="flex flex-col w-full h-[500px] scrollbar-hide overflow-y-auto">
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+
+      <div className="flex py-5 px-2 items-center space-x-5 border-b border-neutral-300 hover:bg-primary-100 cursor-pointer">
+        <input type="radio" name="check" className="w-12 h-8 outline-none border accent-primary-500 cursor-pointer" />
+        <span className="text-md font-normal text-neutral-700 tracking-wider">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </span>
+      </div>
+    </div>
+  );
+};
+export default ListTodos;
